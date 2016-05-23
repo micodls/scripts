5b211234ebbe07de1855809af6b1e8f35ab1b2db
Mico de los Santos <mico.dlsantos@gmail.com>
2016-05-23 13:47:28 +0800

    Script to clone all smartservice recode repos

---
+++ checkout_ssr.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# TODO: setup SSH
+# TODO: check if SSH is already setup
+# TODO: support for HTTP clone
+# TODO: check if repo already cloned
+
+clone SmartService Recode
+git clone ssh://git@10.11.8.1:7999/ssr/apple_push_notification.git
+git clone ssh://git@10.11.8.1:7999/ssr/captive_portal.git
+git clone ssh://git@10.11.8.1:7999/ssr/commons.git
+git clone ssh://git@10.11.8.1:7999/ssr/config_api.gitgit clone
+git clone ssh://git@10.11.8.1:7999/ssr/customer_service_api.git
+git clone ssh://git@10.11.8.1:7999/ssr/default_config_webtool.git
+git clone ssh://git@10.11.8.1:7999/ssr/diameter_rf_plus_server.git
+git clone ssh://git@10.11.8.1:7999/ssr/events_api.git
+git clone ssh://git@10.11.8.1:7999/ssr/file_synchronizer.git
+git clone ssh://git@10.11.8.1:7999/ssr/for_removal_scheduler.git
+git clone ssh://git@10.11.8.1:7999/ssr/for_removal_slow_httplib.git
+git clone ssh://git@10.11.8.1:7999/ssr/google_cloud_messaging.git
+git clone ssh://git@10.11.8.1:7999/ssr/httplib.git
+git clone ssh://git@10.11.8.1:7999/ssr/mobile_application_api.git
+git clone ssh://git@10.11.8.1:7999/ssr/monitor.git
+git clone ssh://git@10.11.8.1:7999/ssr/mui_sms.git
+git clone ssh://git@10.11.8.1:7999/ssr/mui_ussd.git
+git clone ssh://git@10.11.8.1:7999/ssr/notification_engine.git
+git clone ssh://git@10.11.8.1:7999/ssr/ocs.git
+git clone ssh://git@10.11.8.1:7999/ssr/osa.git
+git clone ssh://git@10.11.8.1:7999/ssr/packages_api.git
+git clone ssh://git@10.11.8.1:7999/ssr/please_use_commons_tmp_class_prototypes.git
+git clone ssh://git@10.11.8.1:7999/ssr/smartservice_installers.git
+git clone ssh://git@10.11.8.1:7999/ssr/sms_gateway.git
