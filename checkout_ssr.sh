5f385576324a2fd96cee69c8534e0c25e158d60f
Mico de los Santos <mico.dlsantos@gmail.com>
2016-05-24 12:52:38 +0800

    please_use_commons_tmp_class_prototypes -> for_removal_tmp_class_prototypes

--- checkout_ssr.sh
+++ checkout_ssr.sh
@@ -16,6 +16,7 @@ git clone ssh://git@10.11.8.1:7999/ssr/events_api.git
 git clone ssh://git@10.11.8.1:7999/ssr/file_synchronizer.git
 git clone ssh://git@10.11.8.1:7999/ssr/for_removal_scheduler.git
 git clone ssh://git@10.11.8.1:7999/ssr/for_removal_slow_httplib.git
+git clone ssh://git@10.11.8.1:7999/ssr/for_removal_tmp_class_prototypes.git
 git clone ssh://git@10.11.8.1:7999/ssr/google_cloud_messaging.git
 git clone ssh://git@10.11.8.1:7999/ssr/httplib.git
 git clone ssh://git@10.11.8.1:7999/ssr/mobile_application_api.git
@@ -26,6 +27,5 @@ git clone ssh://git@10.11.8.1:7999/ssr/notification_engine.git
 git clone ssh://git@10.11.8.1:7999/ssr/ocs.git
 git clone ssh://git@10.11.8.1:7999/ssr/osa.git
 git clone ssh://git@10.11.8.1:7999/ssr/packages_api.git
-git clone ssh://git@10.11.8.1:7999/ssr/please_use_commons_tmp_class_prototypes.git
 git clone ssh://git@10.11.8.1:7999/ssr/smartservice_installers.git
 git clone ssh://git@10.11.8.1:7999/ssr/sms_gateway.git
