2aec0f6c1067acc6ca02e172b160df513c535e61
Mico de los Santos <mico.dlsantos@gmail.com>
2016-06-20 10:08:00 +0800

    Added vlc and evernote setup. Added plantuml and java. Added more python dependencies. Added script to update all sub directories

--- osx_yosemite_setup.sh
+++ osx_yosemite_setup.sh
@@ -13,8 +13,13 @@ brew install vim
 brew install redis
 brew install node
 brew install rpm
+brew install plantuml
 
-# Python libraries
+# Python Refactoring
+pip install pylint
+pip install testfixtures
+
+# Python dependencies
 pip install pyyaml
 pip install fluent-logger
 pip install murmur
@@ -25,6 +30,11 @@ pip install zmq
 pip install pysnmp
 pip install cython
 pip install tldextract
+pip install cryptography
+pip install scp
+pip install hashring
+
+# Python documentations
 pip install sphinx
 pip install sphinxcontrib-napoleon
 pip install sphinx_rtd_theme # https://github.com/snide/sphinx_rtd_theme
@@ -35,6 +45,7 @@ curl https://install.meteor.com | sh
 # Other tools
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
+brew cask install java
 brew cask install --appdir="/Applications" sublime-text3
 brew cask install --appdir="/Applications" pycharm-ce
 brew cask install --appdir="/Applications" tunnelblick
@@ -46,6 +57,8 @@ brew cask install --appdir="/Applications" google-drive
 brew cask install --appdir="/Applications" firefox
 brew cask install --appdir="/Applications" spotify
 brew cask install --appdir="/Applications" shiftit
+brew cask install --appdir="/Applications" vlc
+brew cask install --appdir="/Applications" evernote
 
 # Git config
 git config --global user.name "Paolo Miguel M. de los Santos"
@@ -66,3 +79,9 @@ git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Crese
 # echo "HELLO WORLD"
 # alias micovm='ssh -R 52968:localhost:52968 root@10.11.4.17'
 # alias teamvm='ssh root@10.11.90.216'
+# alias ll='ls -la'
+# update_all_subdirectories() {
+#    pushd $1 && find . -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -I {} -n1 sh -c "echo Updating {}; git -C {} checkout master; git -C {} pull --rebase; echo Done; echo ------------ && popd" -- for git > 1.8.5
+#    pushd $1 && find . -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -I {} -n1 sh -c "echo Updating {}; pushd {} && git checkout -f master && git pull --rebase && popd; echo Done; echo ------------" && popd'
+# }
+# alias gitall=update_all_subdirectories
