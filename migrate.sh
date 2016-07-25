f7f05dfcf0d80fdea9de62cda9a16473e588e770
Mico de los Santos <mico.dlsantos@gmail.com>
2016-07-25 17:36:34 +0800

    Fixed migration for vm

--- migrate.sh
+++ migrate.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master`; do
-    IFS='/' read -r -a array <<< "$branch"
-    nb="${array[2]}"
-    git checkout -b $nb "origin/"$nb
-done
-
-# #!/usr/bin/env bash
-
-# for directory in `find . -type d -maxdepth 1 -mindepth 1`; do
-#     pushd $directory
-#     # git remote rm origin
-#     # git remote rename new-origin origin
-#     # git checkout master
-#     # git branch -u origin/master master
-#     # git config --list
-#     # git branch -a
-#     popd
+# for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master`; do
+#     IFS='/' read -r -a array <<< "$branch"
+#     nb="${array[2]}"
+#     git checkout -b $nb "origin/"$nb
 # done
+echo '=============='
+for directory in `find . -maxdepth 1 -mindepth 1 -type d`; do
+    pushd $directory
+    # IFS='/' read -r -a array <<< "$directory"
+    # nb="${array[1]}"
+    # git remote set-url origin git@git.corp.voyager.ph:smartservicerecode/$nb.git
+    # git config --list
+    # git remote rm origin
+    # git remote rename new-origin origin
+    # git checkout master
+    # git branch -u origin/master master
+    # git config --list
+    git branch -a
+    popd
+done
