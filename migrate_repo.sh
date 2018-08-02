8ece9afd52e6bde0064850ea90184ec1e1b6af0d
Mico de los Santos <mico.dlsantos@gmail.com>
2018-08-02 16:18:38 +0800

    Cleanup of migrate repo script

--- migrate_repo.sh
+++ migrate_repo.sh
@@ -3,10 +3,10 @@ old_repo_url=$1
 new_repo_url=$2
 repo=$(basename $old_repo_url)
 
-echo $old_repo_url
-echo $new_repo_url
-echo $repo
-echo $PWD
+# Usage: ./migrate_repo.sh <old url> <new url>
+# Example: ./migrate_repo.sh git@git.corp.voyager.ph:vcm/core-rework.git git@git.corp.voyager.ph:pmdelossantos/core-rework.git
+
+echo "Migrating: $old_repo_url to $new_repo_url"
 
 git clone --mirror $old_repo_url
 pushd $PWD
@@ -16,6 +16,3 @@ git push --mirror origin
 popd
 rm -rf $repo
 popd
-
-# git@git.corp.voyager.ph:vcm/core-rework.git
-# git@git.corp.voyager.ph:pmdelossantos/core-rework.git
